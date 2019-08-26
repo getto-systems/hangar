8f0b9a106e5dd40edb56ad6ab04531e5c15eabf3
shun <shun.fix9@gmail.com>
2019-08-26 13:35:54 +0000

    add: push_latest script

---
+++ getto-hangar-push_latest.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+image=$1
+
+if [ -z "$image" ]; then
+  echo "image not specified"
+  exit 1
+fi
+
+if [ -z "$tag" ]; then
+  echo "tag not specified"
+  exit 1
+fi
+
+echo "target: $image:$tag"
+
+docker pull $image:$tag > /dev/null
+if [ $? == 0 ]; then
+  echo "signed image already pushed"
+  return
+fi
+
+docker pull --disable-content-trust $image:$tag > /dev/null
+if [ $? != 0 ]; then
+  echo "build not finished"
+  return
+fi
+
+export HOME=$(pwd)
+
+key_root=$HOME/.docker/trust/private
+
+mkdir -p $key_root
+
+cat $DOCKER_CONTENT_TRUST_ROOT_KEY > $key_root/$DOCKER_CONTENT_TRUST_ROOT_ID.key
+cat $DOCKER_CONTENT_TRUST_REPOSITORY_KEY > $key_root/$DOCKER_CONTENT_TRUST_REPOSITORY_ID.key
+
+chmod 600 $key_root/*.key
+
+cat $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin && \
+docker tag $image:$tag $image:latest && \
+docker push $image:latest && \
+docker push $image:$tag && \
+:
+
+result=$?
+
+docker logout
+
+if [ $result != 0 ]; then
+  exit 1
+fi
+
+touch push_latest_success
