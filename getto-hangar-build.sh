5fb1418e5b7918e8071c6916b6606cd1637a293b
shun <shun.fix9@gmail.com>
2019-08-29 18:57:49 +0000

    fix: build

--- getto-hangar-build.sh
+++ getto-hangar-build.sh
@@ -8,9 +8,21 @@ image=getto/hangar:$CI_COMMIT_SHORT_SHA
 
 sed -i -e "s|FROM.*|FROM $image|" Dockerfile-test
 
-docker build -t $image . && \
-docker build -t $image-test -f Dockerfile-test --disable-content-trust . && \
-docker run --rm --disable-content-trust $image-test && \
+docker build -t $image .
+
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+if [ -f Dockerfile-test ]; then
+  docker build -t $image-test -f Dockerfile-test --disable-content-trust . && \
+  docker run --rm --disable-content-trust $image-test
+
+  if [ $? != 0 ]; then
+    exit 1
+  fi
+fi
+
 dockle --exit-code 1 $image && \
 trivy --exit-code 1 --quiet --ignore-unfixed --auto-refresh $image && \
 :
