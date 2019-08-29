1c69f7205afe55e17bbd48e78c04419fc34d5355
shun <shun.fix9@gmail.com>
2019-08-29 20:19:47 +0000

    fix: getto-hangar-test

---
+++ getto-hangar-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -x
+
+export HOME=$(pwd)
+
+if [ -z "$image" ]; then
+  echo "image not detected"
+  exit 1
+fi
+
+if [ -f Dockerfile-test ]; then
+  sed -i -e "s|FROM.*|FROM $image|" Dockerfile-test
+
+  docker build -t $image-test -f Dockerfile-test --disable-content-trust . && \
+  docker run --rm --disable-content-trust $image-test
+
+  if [ $? != 0 ]; then
+    exit 1
+  fi
+fi
+
+if [ -f .getto-hangar-trivy-opts ]; then
+  trivy_opts="$(cat .getto-hangar-trivy-opts)"
+else
+  trivy_opts=
+fi
+
+dockle --exit-code 1 $image && \
+trivy --exit-code 1 --quiet --auto-refresh $trivy_opts $image && \
+:
